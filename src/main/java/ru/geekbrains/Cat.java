589791a350827bb1a0fa99637d891fe3af1d1b6b
nolty <nolty81@gmail.com>
2021-11-28 15:39:04 +0500

    Практическое задание к уроку №7

---
+++ src/main/java/ru/geekbrains/Cat.java
@@ -0,0 +1,31 @@
+package ru.geekbrains;
+
+public class Cat {
+    private final String name;
+    private final int appetite;
+    private boolean satiety;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        this.satiety = false;
+    }
+
+    public void eat(Plate p) {
+        if (p.decreaseFood(appetite)) {
+            this.satiety = true;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public boolean isSatiety() {
+        return satiety;
+    }
+}
--- src/main/java/ru/geekbrains/HomeWorkApp.java
+++ src/main/java/ru/geekbrains/HomeWorkApp.java
@@ -1,7 +1,39 @@
 package ru.geekbrains;
 
 public class HomeWorkApp {
-    public static void main(String[] args) {
+    private static final String[] catNameArray = {
+            "Соня", "Клеопатра", "Цунами", "Забияка","Матильда", "Кнопка", "Масяня",
+            "Царапка", "Серсея", "Ворсинка"
+    };
 
+    public static void main(String[] args) {
+        Plate plate = new Plate(100);
+        plate.info();
+
+        Cat[] cats = new Cat[catNameArray.length];
+        for (int i = 0; i < cats.length; i++) {
+            cats[i] = new Cat(catNameArray[i], getRandomNumber(1, 30));
+            System.out.printf("%s имеет аппетит %d ед.\n", cats[i].getName(), cats[i].getAppetite());
+        }
+
+        System.out.println();
+        System.out.println("Кормим котов:");
+        for (Cat cat: cats) {
+            cat.eat(plate);
+            if (cat.isSatiety()) {
+                System.out.printf("%s покушал и сыт\n", cat.getName());
+            } else {
+                System.out.printf("%s не хватило еды и он голоден\n", cat.getName());
+            }
+        }
+
+        System.out.println();
+        System.out.printf("В тарелке осталось %d ед. еды\n", plate.getFood());
+        plate.increaseFood(50);
+        System.out.printf("Добавили еды и в тарелке стало %d ед. еды\n", plate.getFood());
+    }
+
+    private static int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
     }
 }
